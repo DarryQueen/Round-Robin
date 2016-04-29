08349d4d26fe03c333ec984c49d0b9194c8a2386
DarryQueen <slimfrog@gmail.com>
2016-04-29 15:12:30 +0100

    Spotify UID column to users migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160429125345) do
+ActiveRecord::Schema.define(version: 20160429141113) do
 
   create_table "users", force: true do |t|
     t.string   "email",               default: "", null: false
@@ -24,8 +24,10 @@ ActiveRecord::Schema.define(version: 20160429125345) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "spotify_uid",         default: "", null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["spotify_uid"], name: "index_users_on_spotify_uid", unique: true
 
 end
